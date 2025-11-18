040593760ad45bb52adb84cea4b3af29e19a7789
dplmrdp <dplmrdp@gmail.com>
2025-11-18 10:11:17 +0100

    Update update_calendars_federado_multi.js

--- scripts/update_calendars_federado_multi.js
+++ scripts/update_calendars_federado_multi.js
@@ -612,6 +612,8 @@ async function parseFederadoCalendarPage(driver, meta) {
     .addArguments("--window-size=1280,1024")
     .addArguments("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118 Safari/537.36")
     .addArguments(`--user-data-dir=${tmpUserDir}`);
+    .addArguments("--disable-http2")
+
 
   let driver;
   try {
